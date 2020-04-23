a86490934ade18d98e0c8ff9049debaf017ac13c
xvatic <glazkovE@gmail.com>
2020-04-23 23:58:15 +0300

    final

--- src/shapeshooser/Controller.java
+++ src/shapeshooser/Controller.java
@@ -4,6 +4,7 @@ package shapeshooser;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
@@ -75,46 +76,50 @@ public class Controller {
             gc.setLineWidth(5);
             ShapeArrayList<Shape> shapes = new ShapeArrayList<>();
 
-            if(rbLine.isSelected() == true){
-                LineDirect  line = new LineDirect(coordArray[2],coordArray[3],coordArray[0],coordArray[1]);
-                Main.drawShapes(gc,line);
-            }
-
-            if(rbCircle.isSelected() == true){
+            rbCircle.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
                     Circle circle = new Circle(coordArray[2]-coordArray[0],coordArray[0],coordArray[1]);
                     Main.drawShapes(gc,circle);
-            }
-            if(rbSquare.isSelected() == true){
-                    Square square = new Square(coordArray[2]-coordArray[0],coordArray[0],coordArray[1]);
-                    Main.drawShapes(gc,square);
-            }
-            if(rbRectangle.isSelected() == true){
-                    Rectangle_shaped rect = new Rectangle_shaped(coordArray[2]-coordArray[0],coordArray[3]-coordArray[1],coordArray[0],coordArray[1]);
-                    Main.drawShapes(gc,rect);
-            }
-            if(rbOval.isSelected() == true){
-                    Oval oval = new Oval(coordArray[2]-coordArray[0],coordArray[3]-coordArray[1],coordArray[0],coordArray[1]);
-                    Main.drawShapes(gc,oval);
-
                 }
-            if(rbTriangle.isSelected() == true){
-                    Triangle triangle = new Triangle(coordArray[2]-coordArray[0],coordArray[3]-coordArray[1],coordArray[0],coordArray[1]);
-                    Main.drawShapes(gc,triangle);
-            }
+            });
+            rbLine.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        LineDirect  line = new LineDirect(coordArray[2],coordArray[3],coordArray[0],coordArray[1]);
+                        Main.drawShapes(gc,line);
+                    }
+                });
+            rbSquare.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        Square square = new Square(coordArray[2]-coordArray[0],coordArray[0],coordArray[1]);
+                        Main.drawShapes(gc,square);
+                    }
+                });
+            rbRectangle.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        Rectangle_shaped rect = new Rectangle_shaped(coordArray[2]-coordArray[0],coordArray[3]-coordArray[1],coordArray[0],coordArray[1]);
+                        Main.drawShapes(gc,rect);
+                    }
+                });
+            rbOval.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        Oval oval = new Oval(coordArray[2]-coordArray[0],coordArray[3]-coordArray[1],coordArray[0],coordArray[1]);
+                        Main.drawShapes(gc,oval);
+                    }
+                });
+            rbTriangle.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        Triangle triangle = new Triangle(coordArray[2]-coordArray[0],coordArray[3]-coordArray[1],coordArray[0],coordArray[1]);
+                        Main.drawShapes(gc,triangle);
+                    }
+                });
 
-            /*
-            gc.fillPolygon(new double[]{shapes.get(4).getXbegin(),shapes.get(4).getXbegin()-(shapes.get(4).getWidth())/2,shapes.get(4).getXbegin()+(shapes.get(4).getWidth())/2},
-                        new double[]{shapes.get(4).getYbegin(),shapes.get(4).getYbegin()+shapes.get(4).getHeight(),shapes.get(4).getYbegin()+shapes.get(4).getHeight()},
-                        3);
 
-
-            gc.fillRect(shapes.get(0).getXbegin(),shapes.get(0).getYbegin(),shapes.get(0).getWidth(),shapes.get(0).getHeight());
-            gc.fillRect(shapes.get(1).getXbegin(),shapes.get(1).getYbegin(),shapes.get(1).getWidth(),shapes.get(1).getWidth());
-            gc.fillOval(shapes.get(2).getXbegin(),shapes.get(2).getYbegin(),shapes.get(2).getWidth(),shapes.get(2).getHeight());
-
-            gc.fillOval(shapes.get(3).getXbegin(),shapes.get(3).getYbegin(),shapes.get(3).getWidth(),shapes.get(3).getWidth());
-
-            */
             }
         });
 
@@ -128,10 +133,7 @@ public class Controller {
     @FXML
     void initialize() {
 
-
-
-
-
+        draw();
     }
 }
 
